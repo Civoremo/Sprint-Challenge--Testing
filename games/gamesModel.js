bf4d86a2f1791beb6f08e929d21331cf6324455a
Civoremo <omerovic_n@hotmail.com>
2019-02-01 11:44:23 -0500

    inital project set up and commit

---
+++ games/gamesModel.js
@@ -0,0 +1,24 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+    insert,
+    getAll,
+    getById,
+    remove,
+};
+
+async function insert(game) {
+    return db("games").insert(game);
+}
+
+async function getAll() {
+    return db("games");
+}
+
+async function getById(id) {
+    return db("games").where({ id });
+}
+
+async function remove(id) {
+    return null;
+}
